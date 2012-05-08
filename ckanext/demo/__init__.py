f5880aa09609ec1c15df3a00b2970968e6e3b6cb
John Glover <j@johnglover.net>
2012-05-08 14:11:38 +0100

    [templates] add package read template with disqus/social code

--- ckanext/demo/__init__.py
+++ ckanext/demo/__init__.py
@@ -1,1 +1,6 @@
-# package
+try:
+    import pkg_resources
+    pkg_resources.declare_namespace(__name__)
+except ImportError:
+    import pkgutil
+    __path__ = pkgutil.extend_path(__path__, __name__)
